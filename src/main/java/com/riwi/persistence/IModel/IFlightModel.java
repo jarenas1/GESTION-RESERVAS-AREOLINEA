db1d5989a47dc84d8c1c3ab0d2bccd3a43d511bb
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-01 08:11:07 -0500

    Interfaz of PassengerEntity

--- src/main/java/com/riwi/persistence/IModel/IFlightModel.java
+++ src/main/java/com/riwi/persistence/IModel/IFlightModel.java
@@ -4,5 +4,5 @@ import com.riwi.entitie.FlightEntity;
 import com.riwi.persistence.CRUD.*;
 
 //IMPLEMENTAMOS TODOS LOS METODOS QUE VA A TENER EL VUELO
-public interface IFlightModel extends Create<FlightEntity>, Delete<String>, Update<FlightEntity>, Read<String>, ReadAll<FlightEntity> {
+public interface IFlightModel extends Create<FlightEntity>, Delete<Integer>, Update<FlightEntity>, Read<String>, ReadAll<FlightEntity> {
 }
--- src/main/java/com/riwi/persistence/IModel/IPassengerModel.java
+++ src/main/java/com/riwi/persistence/IModel/IPassengerModel.java
@@ -1,4 +1,7 @@
 package com.riwi.persistence.IModel;
 
-public interface IPassengerModel {
+import com.riwi.entitie.PassengerEntity;
+import com.riwi.persistence.CRUD.*;
+
+public interface IPassengerModel extends Create<PassengerEntity>, Read<String>, ReadAll<PassengerEntity>, Update<PassengerEntity>, Delete<Integer> {
 }
