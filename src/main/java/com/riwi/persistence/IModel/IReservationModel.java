ab99072ebb3d70939f80408d7ff1462ad37d9119
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-01 08:20:19 -0500

    Interfaz of ReseravtionEntity

--- src/main/java/com/riwi/persistence/IModel/IReservationModel.java
+++ src/main/java/com/riwi/persistence/IModel/IReservationModel.java
@@ -1,4 +1,10 @@
 package com.riwi.persistence.IModel;
 
-public interface IReservationModel {
+import com.riwi.entitie.ReservationEntity;
+import com.riwi.persistence.CRUD.Create;
+import com.riwi.persistence.CRUD.Delete;
+import com.riwi.persistence.CRUD.Read;
+import com.riwi.persistence.CRUD.ReadAll;
+
+public interface IReservationModel extends Create<ReservationEntity>, Read<Integer,ReservationEntity>, ReadAll<ReservationEntity>, Delete<Integer> {
 }
